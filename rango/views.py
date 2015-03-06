4c1b7e2c050e4c650fd0e84fe71bc94d9858acc4
a <a@Macintosh-25.local>
2015-03-06 15:17:51 -0500

    use template

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,22 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+'''
 def index(request):
     return HttpResponse("Rango says hey there world! <p> <a href='about'>about</a>")
-    
+'''
+
+
+def index(request):
+    context_dict = {'boldmessage' : 'I am bold font from the context'}
+    return render(request, 'rango/index.html', context_dict)
+
+
+'''
 def about(request):
 	html = "Rango says here is the about page. <p> <a href='/rango/'> index </a>"
 	return HttpResponse(html)
+'''
+
+def about(request):
+    return render(request, 'rango/about.html')
